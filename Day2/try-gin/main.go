06cc719c09a33e1e6657a0859458ccc3f432a584
Hilmaww <hilmi.ambari27@gmail.com>
2022-10-26 23:13:36 +0700

    add feature get album by id

--- Day2/try-gin/main.go
+++ Day2/try-gin/main.go
@@ -18,6 +18,7 @@ type album struct {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
@@ -37,6 +38,7 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+// fungsi POST untuk menuju albums
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
@@ -46,4 +48,19 @@ func postAlbums(c *gin.Context) {
 
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusOK, newAlbum)
+}
+
+// Memanggil fungsi get berdasarkan id
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// loop over list of Albums sampe ketemu album yang cocok dengan id
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album with the id inserted is not found"})
 }
