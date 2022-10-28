7a49e8521d245726719c30fe7c2f6f3a81b0bb66
Hilmaww <hilmi.ambari27@gmail.com>
2022-10-28 23:12:32 +0700

    algoritma petani😎

--- Day5/aqua-tugas-5/main.go
+++ Day5/aqua-tugas-5/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 type point struct {
@@ -14,9 +11,9 @@ type point struct {
 }
 
 func main() {
-	bufferReader := bufio.NewReader(os.Stdin)
-	//uangTotal := 100000
-	var inputHargaBeli string
+	//bufferReader := bufio.NewReader(os.Stdin)
+	uangTotal := 100000
+	// var inputHargaBeli string
 
 	// points := []point{
 	// 	{ID: 1, barang: "Benih Lele", harga: 50000},
@@ -30,40 +27,54 @@ func main() {
 	// 	{ID: 9, barang: "Benih Cupang", harga: 10000},
 	// 	{ID: 10, barang: "Probiotik B", harga: 10000},
 	// }
-	fmt.Print("Enter: ")
-	inputHargaBeli, err := bufferReader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
+
+	// fmt.Print("Enter: ")
+	// inputHargaBeli, err := bufferReader.ReadString('\n')
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// hasil := parser(inputHargaBeli)
+
+	//HARDCODEEE
+	point_choices := []point{
+		{ID: 2, barang: "Pakan lele", harga: 25000},
+		{ID: 3, barang: "Probiotik A", harga: 75000},
 	}
-	hasil := parser(inputHargaBeli)
-	fmt.Print(hasil)
+
+	canBuy, sum := farmerBuy(point_choices, uangTotal)
+
+	if !canBuy {
+		fmt.Println("Petani tidak dapat membeli barang yang telah dipilih")
+	}
+	fmt.Printf("Petani berhasil membeli dengan total belanjaan %d", sum)
+
 }
 
-func parser(s string) []int {
-	nums := []int{}
-	numWhile := []int{}
-	for i := 0; i < len(s); i++ {
-		if s[i] != 32 {
-			numDumm, _ := strconv.Atoi(string(s[i]))
-			numWhile = append(numWhile, numDumm)
-		} else {
-			numDumm, _ := strconv.Atoi(string(s[i]))
-			nums = append(nums, numDumm)
-		}
-	}
-	return nums
-}
+// func parser(s string) []int {
+// 	nums := []int{}
+// 	numWhile := []int{}
+// 	for i := 0; i < len(s); i++ {
+// 		if s[i] != 32 {
+// 			numDumm, _ := strconv.Atoi(string(s[i]))
+// 			numWhile = append(numWhile, numDumm)
+// 		} else {
+// 			numDumm, _ := strconv.Atoi(string(s[i]))
+// 			nums = append(nums, numDumm)
+// 		}
+// 	}
+// 	return nums
+// }
 
 // desc: menentukan apakah barang yang dipilih dapat dibeli atau engga
 // input: struct point
 // output: boolean, true bila boleh, false jika gaboleh
 // syarat: hanya boleh membeli 1 item masing masing barang
 //      	dan jumlah total modal adalah 100.000
-func farmerBuy(item []point) (bool, int) {
+func farmerBuy(item []point, modal int) (bool, int) {
 	totalBeli := 0
 	for i := 0; i < len(item); i++ {
 		totalBeli += item[i].harga
-		if totalBeli > 100000 {
+		if totalBeli > modal {
 			return false, totalBeli
 		}
 	}
