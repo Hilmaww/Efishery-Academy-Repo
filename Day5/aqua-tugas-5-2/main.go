eda9f2267e97f80e652e527b86c0582a661ca6d7
Hilmaww <hilmi.ambari27@gmail.com>
2022-10-29 21:01:46 +0700

    penyelesaiian feat aggregat

--- Day5/aqua-tugas-5-2/main.go
+++ Day5/aqua-tugas-5-2/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sync"
+	"time"
 )
 
 type Desc struct {
@@ -14,6 +16,10 @@ type Job struct {
 	Title      string `json:"title"`
 	WorkFrom   string `json:"work_from"`
 	Department string `json:"department"`
+}
+type Aggr struct {
+	Desc Desc
+	Jobs []Job
 }
 
 func fetch(url string, d interface{}) error {
@@ -31,21 +37,55 @@ func fetch(url string, d interface{}) error {
 		return err
 	}
 	return err
+}
 
+// Mencoba menghitung waktu
+func caculateTime(start time.Time) {
+	fmt.Println("dari calculate ", start)
+	fmt.Printf("took %v\n", time.Since(start))
+}
+
+// mencoba agregasi
+func Aggregat() (Aggr, error) {
+	defer caculateTime(time.Now())
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	var desc Desc
+	var descErr error
+	var jobs []Job
+	var jobsErr error
+
+	go func() {
+		descErr = fetch("https://workspace-rho.vercel.app/api/description", &desc)
+		wg.Done()
+	}()
+	if descErr != nil {
+		return Aggr{}, descErr
+	}
+	go func() {
+		jobsErr = fetch("https://workspace-rho.vercel.app/api/description", &desc)
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	if jobsErr != nil {
+		return Aggr{}, jobsErr
+	}
+
+	aggr := Aggr{
+		Desc: desc,
+		Jobs: jobs,
+	}
+	return aggr, nil
 }
 
 func main() {
-	var desc Desc
-	var jobs []Job
-	err := fetch("https://workspace-rho.vercel.app/api/description", &desc)
-	if err != nil {
-		panic(err)
-	}
-	err = fetch("https://workspace-rho.vercel.app/api/jobs", &jobs)
-	if err != nil {
-		panic(err)
-	}
 
-	fmt.Printf("%+v\n", desc)
-	fmt.Printf("%+v\n", jobs)
+	aggr, err := Aggregat()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", aggr)
 }
